91f79c26de9ae76177b27bdfeabe8d8a779b499d
pocycle <pocycle@gmail.com>
2025-07-29 15:47:54 -0600

    TO REVERT. Comment out broken program from callback change

--- SerialPrograms/Source/PokemonLGPE/PokemonLGPE_Panels.cpp
+++ SerialPrograms/Source/PokemonLGPE/PokemonLGPE_Panels.cpp
@@ -11,7 +11,7 @@
 
 #include "Programs/Farming/PokemonLGPE_DailyItemFarmer.h"
 #include "Programs/ShinyHunting/PokemonLGPE_AlolanTrade.h"
-#include "Programs/ShinyHunting/PokemonLGPE_FossilRevival.h"
+// #include "Programs/ShinyHunting/PokemonLGPE_FossilRevival.h"
 #include "Programs/ShinyHunting/PokemonLGPE_GiftReset.h"
 #include "Programs/ShinyHunting/PokemonLGPE_LegendaryReset.h"
 
@@ -36,7 +36,7 @@ std::vector<PanelEntry> PanelListFactory::make_panels() const{
 
     ret.emplace_back("---- Shiny Hunting ----");
     ret.emplace_back(make_single_switch_program<AlolanTrade_Descriptor, AlolanTrade>());
-    ret.emplace_back(make_single_switch_program<FossilRevival_Descriptor, FossilRevival>());
+    // ret.emplace_back(make_single_switch_program<FossilRevival_Descriptor, FossilRevival>());
     ret.emplace_back(make_single_switch_program<GiftReset_Descriptor, GiftReset>());
     ret.emplace_back(make_single_switch_program<LegendaryReset_Descriptor, LegendaryReset>());
 
